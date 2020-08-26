f826a4071fbaf27f5e7d3189433e018475bfebd2
Rajshekar Eashwarappa <reashw@codeaurora.org>
2020-08-26 18:39:45 +0530

    sdm660-common: Remove vibrator HAL interface
    Since we are moving to vendor vibrator HAL
    Remove the existing HIDL based vibrator HAL
    
    CRs-Fixed: 2764383
    Change-Id: If95ce404a2c2a72a1a1beecf915db38c15a5ff8d

--- sdm660.mk
+++ sdm660.mk
@@ -414,11 +414,6 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl:64 \
-    android.hardware.vibrator@1.0-service
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
