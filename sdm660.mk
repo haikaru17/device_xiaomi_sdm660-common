e9fa2824df56366b2ad327251ab52bfaf2aad24b
orgesified <gesishabani1@gmail.com>
2020-10-21 15:50:13 +0200

    sdm660-common: add privapp permission for google restore
    *gapps like FlameGApps haven't yet added causing device to be stuck on
    bootanimation
    
    Signed-off-by: orgesified <gesishabani1@gmail.com>
    Change-Id: I5135e89c22fb24a393ae4f867222f64806b4056c

--- sdm660.mk
+++ sdm660.mk
@@ -23,7 +23,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/privapp-permissions-google-restore.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-google-restore.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
