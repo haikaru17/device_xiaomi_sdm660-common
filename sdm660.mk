5954a912dc4ebfb22bd68e6dd8daec127be32e11
ZeeLog <Danjel.Zammit@gmail.com>
2021-02-12 10:46:36 +0100

    sdm660-common: Build vendor variant of vendor.display.config@1.0
    * Fixes QSEECOMD: : Init dlopen(libsecureui.so, RLTD_NOW) is failed.... dlopen failed: library "vendor.display.config@1.0.so" not found: needed by /vendor/lib64/libsecureui.so in namespace (default)

--- sdm660.mk
+++ sdm660.mk
@@ -182,6 +182,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
+    vendor.display.config@1.0.vendor \
     vendor.display.config@1.1
 
 # DPM
