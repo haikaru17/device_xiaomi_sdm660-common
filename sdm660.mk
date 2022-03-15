c6e28048a037f18bfd094ddefe446ef34ac35929
RONIN808 <shubhamshedage808@gmail.com>
2022-03-15 22:09:55 +0530

    sdm660-common: Update Bluetooth from `LA.UM.9.2.r1-03500-SDMxx0.0`
    * From ddv_sprout-m690-user-11-RKQ1.210607.001-00WW_3_490-release-keys
    
    Signed-off-by: harshhaareddy <harshhaa03@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -110,7 +110,8 @@ RELAX_USES_LIBRARY_CHECK := true
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
