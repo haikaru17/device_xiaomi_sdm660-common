5ee998ddda8ff2cb8d5749ad2f50480b7acaf69d
haikaru17 <haikalluthfianinobalukia@proton.me>
2025-10-10 16:46:26 +0700

    sdm660-common: Build `android.hidl.allocator@1.0` * Could not load /vendor/lib64/hw/vendor.qti.hardware.qseecom@1.0-impl.so from default namespace: dlopen failed: library "android.hidl.allocator@1.0.so" not found: needed by /vendor/lib64/hw/vendor.qti.hardware.qseecom@1.0-impl.so in namespace (default).
    Signed-off-by: haikaru17 <haikalluthfianinobalukia@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -234,6 +234,8 @@ endif
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.allocator@1.0 \
+    android.hidl.allocator@1.0.vendor \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
     libhidltransport \
